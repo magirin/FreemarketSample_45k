a3d94e1341e783558ee4c46483357a988bde98a2
MAGIJIG <magijigu@gmail.com>
2019-07-06 22:00:03 +0900

    add devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root 'products#index'
   resources :products, only: [:show, :index, :create]
   resources :users, only: [:edit] do
