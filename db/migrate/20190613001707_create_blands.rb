b65cb05433891b6b1796f54a835ff0621be6aa4d
MAGIJIG <magijigu@gmail.com>
2019-07-13 19:13:50 +0900

    delete parents id from blands table

--- db/migrate/20190613001707_create_blands.rb
+++ db/migrate/20190613001707_create_blands.rb
@@ -1,7 +1,6 @@
 class CreateBlands < ActiveRecord::Migration[5.0]
   def change
     create_table :blands do |t|
-      t.integer :parent_id, null: true
       t.string :name, null: false, index: true
       t.timestamps
     end
