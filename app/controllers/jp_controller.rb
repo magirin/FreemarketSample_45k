56c12924e7e14c96d5f23a82565ea589b2cf5311
lpgmsw <leaningteexpc@gmail.com>
2019-07-28 14:43:37 +0900

    fix_each_of_pages

--- app/controllers/jp_controller.rb
+++ app/controllers/jp_controller.rb
@@ -1,5 +1,6 @@
 class JpController < ApplicationController
   def index
     @products = Product.where("category_id=1").order("created_at DESC").page(params[:page]).per(4)
+    @category = Category.all
   end
 end
